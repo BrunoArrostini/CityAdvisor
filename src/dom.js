7921dc45c3b329f5b86cc6739bc1ac148f730729
BrunoArrostini <email@github.com>
2023-01-19 13:20:42 +0100

    fetch working

--- src/dom.js
+++ src/dom.js
@@ -9,7 +9,7 @@ export const imgContainer = document.getElementsByClassName("img-container");
 //Form
 export const form = document.getElementById("form");
 export const inputText = document.getElementById("inputText");
-export const errorBox = form.lastElementChild;
+export const errorBox = document.getElementById("errorBox");
 //Button
 export const btnSubmit = document.getElementById("btn");
 //Results
--- src/functions.js
+++ src/functions.js
@@ -1,55 +1,21 @@
 import axios from "axios";
-import { inputText, btnSubmit, errorBox, form, cityScore, cityInfo, cityCategories} from "./dom";
+import { inputText, errorBox, form, cityScore, cityInfo, cityCategories} from "./dom";
 
 form.addEventListener("submit", function(e){
     e.preventDefault();
     cityInfo.innerHTML = "";
     cityCategories.innerHTML = "";
     getCity(inputText.value);
-    /*if (getCity()=false){
-        showError();
-    }*/
 });
 
 export async function getCity(city){
-    
-        const res = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${city.toLowerCase().trim().replaceAll(' ', '-')}/scores/`)
-        .then (res =>{
-          console.log(res)  
-        })
-
+    const res = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${city.toLowerCase().trim().replaceAll(' ', '-')}/scores/`)
+    .then (res =>{
+        cityScore.innerHTML = res.data.teleport_city_score.toFixed(2);
+        cityInfo.innerHTML = res.data.summary;
+        //cityCategories.innerHTML = res.data.categories;
+        }) 
+    .catch (err =>{ 
+        errorBox.innerHTML= "Please retry <br> city name must be in english";
+    });  
 }
-
-cityCategories.forEach((e, i) => {
-    const info = document.createElement("div");
-    info.id = `cat${i}`;
-    info.textContent = `${e.name}:${e.score_out_of_10.toFixed(2)}`;
-    info.setAttribute("style",`color:${e.color}`);
-    cityCategories.append(info);
-});
-
-cityScore.forEach((e)=>{
-    const score = document.createElement("div");
-    score.textContent = `${e.teleport_city_score.toFixed(2)}`;
-    cityScore.append(score);
-});
-
-cityInfo.forEach((e)=>{
-    const infos = document.createElement("div");
-    infos.textContent = `${e.summary}`;
-    cityInfo.append(infos);
-});
-/*export function showError(err){
-    let errorMessage = document.createElement("p");
-    errorBox.append(errorMessage);
-    
-
-    if (inputText.value = ""){
-        errorBox.style.display= "none";
-    } else {
-        errorBox.style.display= "block";
-    }
-}*/
-
-
-
