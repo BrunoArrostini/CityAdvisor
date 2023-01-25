849497dacdcbb5267bab7a9a6965bbe2c27a16d3
BrunoArrostini <email@github.com>
2023-01-25 12:51:05 +0100

    media query

--- src/functions.js
+++ src/functions.js
@@ -11,17 +11,21 @@ form.addEventListener("submit", function(e){
 export async function getCity(city){
     const res = await axios.get(`https://api.teleport.org/api/urban_areas/slug:${city.toLowerCase().trim().replaceAll(' ', '-')}/scores/`)
     .then (res =>{
-        cityScore.innerHTML = res.data.teleport_city_score.toFixed(2);       
-        cityInfo.innerHTML = "CITY INFO:" + res.data.summary;
+        cityScore.innerHTML = "City score: " + res.data.teleport_city_score.toFixed(2);       
+        cityInfo.innerHTML =  res.data.summary;
         res.data.categories.forEach((e, i) => {
             const elem = document.createElement("div");
             elem.id = `cat${i}`;
             elem.textContent = `${e.name}: ${e.score_out_of_10.toFixed(2)}`;
             cityCategories.appendChild(elem);
             cityCategories.style.overflow="scroll";
+            cityCategories.style.borderColor="rgb(249, 255, 127)";
+            cityCategories.style.borderStyle="inset";
         });           
     })
     .catch (err =>{ 
         errorBox.innerHTML= "Please retry <br> city name must be in english";
     });  
 }
+
+
